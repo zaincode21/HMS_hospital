3bc7992f310f446ee303875ffa3e367d4d96c809
zaincode21 <sergedukuziyaremye@gmail.com>
2025-04-08 21:35:44 +0200

    this code is coded by zaincode21

---
+++ hospitalManagementSystem/src/hospitalmanagementsystem/FXMLDocumentController.java
@@ -0,0 +1,128 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hospitalmanagementsystem;
+
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ *
+ * @author serge
+ */
+public class FXMLDocumentController implements Initializable {
+    
+  
+    @FXML
+    private CheckBox login_fcheckBox;
+
+    @FXML
+    private AnchorPane login_form;
+
+    @FXML
+    private Button login_loginBtn;
+
+    @FXML
+    private PasswordField login_password;
+
+    @FXML
+    private Hyperlink login_registerHere;
+
+    @FXML
+    private TextField login_usename;
+
+    @FXML
+    private ComboBox<?> login_user;
+
+    @FXML
+    private AnchorPane main_form;
+
+    @FXML
+    private CheckBox register_checkBox;
+
+    @FXML
+    private TextField register_email;
+
+    @FXML
+    private AnchorPane register_form;
+
+    @FXML
+    private Hyperlink register_loginHere;
+
+    @FXML
+    private PasswordField register_password;
+
+    @FXML
+    private TextField register_showPassword;
+
+    @FXML
+    private Button register_signupBtn;
+
+    @FXML
+    private TextField register_username;
+    
+      // DATABASE TOOLS 
+    private Connection connect;
+    private PreparedStatement prepare;
+    private ResultSet result;
+    
+   
+    private final AlertMessage alert = new AlertMessage();
+    
+    public void registerAccount(){
+        if(register_email.getText().isEmpty()
+                || register_username.getText().isEmpty()
+                || register_password.getText().isEmpty()){
+            alert.errorMessage("please fill all fields");  
+        }else{
+            String checkUsername = "SELECT * FROM admin WHERE username = ? AND password = ?";           
+            connect = Database.connectDB();
+            
+            try{
+                prepare = connect.prepareStatement(checkUsername);
+                result = prepare.executeQuery();
+                
+                if(result.next()){
+                    alert.errorMessage(register_username.getText()+"is already exist!");
+                }else{
+                    String insertData ="INSERT INTO admin (email,username,password,date) values(?,?,?,?)";
+                }
+            }catch(Exception e) {e.printStackTrace();}
+        }
+        
+    }
+      
+    public void switchForm(ActionEvent event){
+        
+    if(event.getSource() == login_registerHere){
+        //REGISTRATION FORM WILL SHOW
+        login_form.setVisible(false);
+        register_form.setVisible(true);
+    }else if(event.getSource() == register_loginHere){
+        //LOGIN FORM WILL SHOW
+        login_form.setVisible(true);
+        register_form.setVisible(false);
+    }
+    }
+   @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    } 
+    
+}
